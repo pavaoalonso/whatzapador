3980a634d937f193850c2800238ea2622fb2fef4
Paulo Alonso <alopaulohenrique@gmail.com>
2021-03-08 23:49:22 -0300

    foi

--- src/js/whatzapador.js
+++ src/js/whatzapador.js
@@ -3,12 +3,15 @@ $("#btnAbrirChat").click((event) => {
     event.preventDefault();
 
     let numeroCelular = this.numeroCelular.value;
-    let link = "https://api.whatsapp.com/send?phone=55";
+    let link = "https://web.whatsapp.com/send?phone=55";
     let linkCompleto = link+numeroCelular;
 
     //console.log(linkCompleto);
 
     //console.log($("iframe").attr('src', linkCompleto));
+    // console.log('<object data="'+linkCompleto+'"/>');   
+
+    // $("#mydiv").html('<object data="'+linkCompleto+'"/>');
 
     window.open(linkCompleto);
 });
